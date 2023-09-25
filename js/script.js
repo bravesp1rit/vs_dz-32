bc4ea6502e188c516b9abbc1a3e4c68875ca4d8f
brave <progpavlovmark@gmail.com>
2023-09-25 15:33:45 +0300

    added number check

--- js/script.js
+++ js/script.js
@@ -7,14 +7,19 @@ let calculator = {
         this.number2 = num2;
     },
     sum(){
+        if ((typeof this.number1 !== 'number') || (typeof this.number2 !== 'number')){
+            return 'error, not a number';
+        }
         return this.number1 + this.number2;
     },
     mul(){
+        if ((typeof this.number1 !== 'number') || (typeof this.number2 !== 'number')){
+            return 'error, not a number';
+        }
         return this.number1 * this.number2;
     }
 }
-calculator.read(3, 2);
-console.log(calculator);
+calculator.read(33, 2);
 console.log(calculator.sum());
 console.log(calculator.mul());
 
